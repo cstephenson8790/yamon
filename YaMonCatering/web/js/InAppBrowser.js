d14185f80aeea52fe414b5f8a65fb5a658a01089
cstephenson8790 <cstephenson8790@conestogac.on.ca>
2016-04-03 04:23:21 -0400

    Added files via upload

---
+++ YaMonCatering/web/js/InAppBrowser.js
@@ -0,0 +1,8 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+var ref = window.open(encodeURI('https://www.facebook.com/YaMonCatering/'), '_blank', 'location=yes');
+var ref1 = window.open(encodeURI('https://twitter.com/Ya_Mon_Catering'), '_blank', 'location=yes');
+var ref2 = window.open(encodeURI('https://www.instagram.com/YaMonCatering/'), '_blank', 'location=yes');
+var ref3 = window.open(encodeURI('http://yamoncatering.blogspot.ca/'), '_blank', 'location=yes');
+ref = window.open('next.html', '_self');
+    }
